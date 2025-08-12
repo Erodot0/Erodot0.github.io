a393c6b81a27b49006876ed06620147e95417b85
Janmanpreet Singh <106154803+Erodot0@users.noreply.github.com>
2025-08-12 12:49:57 +0200

    chore: remove jpg assets and use placeholders

--- docs/script/modules/ui/projectsInjector.js
+++ docs/script/modules/ui/projectsInjector.js
@@ -9,12 +9,13 @@ projects.forEach((project) => {
     .join('');
   const code = `
     <div class="project">
+      <img class="project--image" src="${project.img}" alt="${project.title} screenshot">
       <h3 class="project--title">${project.title}</h3>
       <p class="project--description">${project.description}</p>
       <div class="project--stack">${badges}</div>
       <div class="project--links">
-        <a href="${project.github}" target="_blank">GitHub</a>
-        <a href="${project.live}" target="_blank">Live</a>
+        <a class="btn btn--ghost" href="${project.github}" target="_blank">GitHub</a>
+        <a class="btn btn--solid" href="${project.live}" target="_blank">Live</a>
       </div>
       <p class="project--outcome">${project.outcome}</p>
     </div>
