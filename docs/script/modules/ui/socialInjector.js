ffdd9b4b320c933930bce97a61a3f4fde4aa596e
Janmanpreet Singh <106154803+Erodot0@users.noreply.github.com>
2025-08-12 13:54:57 +0200

    Refactor skill carousel and injectors

--- docs/script/modules/ui/socialInjector.js
+++ docs/script/modules/ui/socialInjector.js
@@ -2,27 +2,21 @@
 import { socials } from '../data/database.js';
 
 const socialWrapper = document.querySelector('.contactContainer--social');
-const fragment = document.createDocumentFragment();
+const template = document.getElementById('social-template');
 
-socials.forEach((social) => {
-  const li = document.createElement('li');
-  li.className = 'contactContainer--social__item';
-
-  const a = document.createElement('a');
-  a.href = social.href;
-  a.target = '_blank';
-
-  const icon = document.createElement('div');
-  icon.className = 'socialIcon';
-  const img = document.createElement('img');
-  img.src = social.src;
-  img.alt = `${social.name} icon`;
-  icon.appendChild(img);
-
-  a.append(icon, document.createTextNode(social.name));
-  li.appendChild(a);
-  fragment.appendChild(li);
-});
-
-socialWrapper.appendChild(fragment);
+if (socialWrapper && template) {
+  const frag = document.createDocumentFragment();
+  socials.forEach((social) => {
+    const el = template.content.cloneNode(true);
+    const a = el.querySelector('a');
+    a.href = social.href;
+    a.target = '_blank';
+    const img = el.querySelector('img');
+    img.src = social.src;
+    img.alt = `${social.name} icon`;
+    el.querySelector('.socialName').textContent = social.name;
+    frag.appendChild(el);
+  });
+  socialWrapper.appendChild(frag);
+}
 
